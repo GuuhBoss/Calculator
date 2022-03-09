8044f3c7c7ba9120b1d292b8a972575c8fb5fecd
GuuhBoss <gustavosjob@gmail.com>
2022-03-09 03:24:33 +0000

    minor bugs

--- calculadora.js
+++ calculadora.js
@@ -1,9 +1,15 @@
+function calcular(resultado) {
+  document.getElementsByTagName("input")[0].value = eval(resultado);
+  console.log(eval(resultado));
+  // alert('Sua conta é igual a: ' + eval(resultado) )
+  // document.getElementsByTagName('input')[0].value = ""
+}
+function valor(num) {
+  num1 = document.getElementsByTagName("input")[0].value += num;
+  //  console.log(num1)
+  return num1;
+}
 
-
-function valor(id) {
-
-	 var x = document.getElementById('ecra')
-	 x.append.className
-
-	alert(id)
+function apagar() {
+  document.getElementsByTagName("input")[0].value = "";
 }
